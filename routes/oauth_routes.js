524d95d42115e5e34d6a7a7a234c7a21208d658e
clintonjnelson <clintonjnelson@live.com>
2015-08-17 14:09:59 -0700

    add ability to link signs from prior accounts to a new account

--- routes/oauth_routes.js
+++ routes/oauth_routes.js
@@ -1,11 +1,13 @@
 'use strict';
 
-var bodyparser = require('body-parser');
-var setCors    = require('../lib/routes_middleware/set_cors_permissions.js');
+var bodyparser           = require('body-parser');
+var setCors              = require('../lib/routes_middleware/set_cors_permissions.js');
+var removePassptSessions = require('../lib/routes_middleware/remove_passport_sessions.js');
 
 
 module.exports = function(router, passport) {
   router.use(bodyparser.json());
+  router.use(removePassptSessions);  // bypass oauth1 sessions
 
   // Require routes by provider
   require('./oauth_routes/facebook.js')(router, passport);
